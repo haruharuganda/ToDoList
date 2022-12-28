07d330f0940cb43fdb9dd425aa497484b362913f
Ju <97508841+haruharuganda@gmail.com>
2022-12-28 18:14:47 +0900

    빈칸리셋

--- src/components/head.js
+++ src/components/head.js
@@ -6,26 +6,43 @@ const Head = ({ cards, setCards }) => {
   const [content, setContent] = useState("");
 
   const addCardHandler = () => {
-    let working = true;
     const newCards = {
       id: cards.length + 1,
       title: title,
       content: content,
-      working: working,
+      working: true,
     };
 
     setCards([...cards, newCards]);
+    setTitle("");
+    setContent("");
   };
 
   return (
     <header className="header">
       <div className="input-container">
-        <Input current={title} seting={setTitle}>
-          제목
-        </Input>
-        <Input current={content} seting={setContent}>
-          내용
-        </Input>
+        <div className="input-container">
+          <div className="input-group">
+            <label className="input-label">제목</label>
+            <input
+              type={"text"}
+              className="add-input"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            ></input>
+          </div>
+        </div>
+        <div className="input-container">
+          <div className="input-group">
+            <label className="input-label">내용</label>
+            <input
+              type={"text"}
+              className="add-input"
+              value={content}
+              onChange={(e) => setContent(e.target.value)}
+            ></input>
+          </div>
+        </div>
       </div>
       <div className="button">
         <button onClick={addCardHandler}>추가하기</button>
